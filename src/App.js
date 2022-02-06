1b2db6e070a331e64b428b611d8946ca0e9a6c86
Osamudiamen <osas.azamegbe@gmail.com>
2022-02-06 13:48:28 +0000

    add balance and players to dapp state

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import React, {useState, useEffect} from 'react';
 
 function App() {
   const [manager, setManager] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState(0.0);
 
   const lottery = new Lottery();
 
   useEffect(() => {
     const loadLotteryState = async () => {
-      const contractManager = await lottery.getManager();
-      setManager(contractManager);
+      setManager(await lottery.getManager());
+      setPlayers(await lottery.getPlayers());
+      setBalance(await lottery.getBalance());
     }
     
     loadLotteryState();
@@ -22,6 +25,7 @@ function App() {
     <div className="App">
       <h1>Lottery DApp</h1>
       <p>The Manager of this Lottery contract is <strong>{manager}</strong></p>
+      <p>There are currently <strong>{players.length}</strong> players, with a total lottery pot of <strong>{balance}</strong> ether.</p>
     </div>
   );
 }
